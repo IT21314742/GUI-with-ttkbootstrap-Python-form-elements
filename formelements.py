6f192010f98ebc1a3f60136ae18aa2fa3b7b3b48
IT21314742 <it21314742@my.sliit.lk>
2024-09-29 16:08:42 +0530

    Update
    Update

--- formelements.py
+++ formelements.py
@@ -4,12 +4,13 @@ import ttkbootstrap as tb
 root = tb.Window(themename="superhero")
 
 #root = tk()
-root.title()("TTK Bootstrap!")
+root.title("TTK Bootstrap!")
 # root.iconbitmap('images/codemy.ico')
 root.geometry('500x350')
 
 
 # Label
+my_label = tk.Label(root, text="Click the checkbox below if you are not robot", font=("Poppins", 30))
 
 # CheckButton
 
