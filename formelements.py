6de6c029383925db600fb270e39c10ac1d27fd01
IT21314742 <it21314742@my.sliit.lk>
2024-09-29 16:19:16 +0530

    Update
    Update

--- formelements.py
+++ formelements.py
@@ -8,6 +8,9 @@ root.title("TTK Bootstrap!")
 # root.iconbitmap('images/codemy.ico')
 root.geometry('800x550')
 
+def fname(arg):
+    pass
+
 
 # Label
 my_label = tb.Label(root, text="Click the checkbox below if you are not robot", font=("Poppins", 15))
@@ -16,7 +19,7 @@ my_label.pack(pady=(40,10))
 # CheckButton
 var1 = IntVar()
 my_check = tb.Checkbutton(bootstyle="primary", text="Check Me Out!", variable=var1, onvalue=1, offvalue=0, command=checker )
-
+my_check.pack(pady=10)
 
 # Round Toggle Button
 
